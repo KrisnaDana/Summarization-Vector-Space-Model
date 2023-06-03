c6c2c27275c70eb9af1f320a4a47dbf755b24137
Krisna Dana <krisnadanaofficial@gmail.com>
2023-06-03 14:03:29 +0800

    implementation success

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
 import summary
 
+# flask --debug run
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
+@app.route('/', methods=['POST'])
 def submit():
-    judul = request.form['judul']
-    teks = request.form['teks']
-    # tes = summary.main(judul, teks)
-    return f"{judul}, {teks}!"
+    title = request.form['judul']
+    text = request.form['teks']
+    count = request.form['jumlah']
+    result = summary.main(title, text, count)
+    return render_template('index.html', judul=title, teks=text, jumlah=count, hasil=result)
--- summary.py
+++ summary.py
@@ -1,9 +1,106 @@
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
-from nltk.cluster.util import cosine_distance
 import re
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
-def main(judul, teks):
-    return "tes"
+
+def real_text_preprocessing(text):
+    return sent_tokenize(text)
+
+def title_preprocessing(title):
+    # LOWERCASE + TOKENIZE TO SENTENCES + REMOVE PUNCTUATION + REMOVE NUMBER
+    title = title.lower()
+    title = re.sub(r'[^\w\s]','',title) #Remove punctuation
+    title = re.sub(r'\w*\d\w*','',title) #Remove number
+
+    # STOPWORDS REMOVAL
+    factory = StopWordRemoverFactory()
+    stopword = factory.create_stop_word_remover()
+    title = stopword.remove(title)
+
+    #STEMMING
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    title = stemmer.stem(title)
+
+    #TOKENIZE TO WORDS
+    words = word_tokenize(title)
+
+    #REMOVE DUPLICATE WORDS
+    words = set(words)
+
+    return words
+
+def text_preprocessing(text):
+    # LOWERCASE + TOKENIZE TO SENTENCES + REMOVE PUNCTUATION + REMOVE NUMBER
+    text = text.lower()
+    sentences = sent_tokenize(text)
+    temp = []
+    for sentence in sentences:
+        sentence = re.sub(r'[^\w\s]','',sentence) #Remove punctuation
+        sentence = re.sub(r'\w*\d\w*','',sentence) #Remove number
+        if sentence != None:
+            temp.append(sentence)
+        else:
+            temp.append(".")
+    sentences = temp
+
+    # STOPWORDS REMOVAL
+    factory = StopWordRemoverFactory()
+    stopword = factory.create_stop_word_remover()
+    temp = []
+    for sentence in sentences:
+        sentence = stopword.remove(sentence)
+        temp.append(sentence)
+    sentences = temp
+
+    #STEMMING
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    temp = []
+    for sentence in sentences:
+        sentence = stemmer.stem(sentence)
+        temp.append(sentence)
+    sentences = temp
+
+    #TOKENIZE TO WORDS
+    words = [word_tokenize(word) for word in sentences]
+
+    #REMOVE DUPLICATE WORDS
+    words = [set(word) for word in words]
+
+    return words
+
+def jaccard_similarity(title, text):
+    # CALCULATE JACCARD SIMILARITY
+    temp = []
+    for t in text:
+        if(t == set()):
+            temp.append(0)
+            continue
+        j = float(len(title.intersection(t))) / len(title.union(t))
+        temp.append(j)
+    return enumerate(temp)
+
+def similarity_ranking(similarity):
+    return sorted(similarity,key=lambda s:s[1],reverse=True)
+
+def result(ranking, real_text, count):
+    temp = []
+    for i in range(int(count)):
+        for j, rank in enumerate(ranking):
+            if i == j:
+                temp.append(real_text[rank[0]])
+                break
+    result = ' '.join([str(t) for t in temp])
+    return result
+
+def main(title, text, count):
+    real_text = real_text_preprocessing(text)
+    title = title_preprocessing(title)
+    text = text_preprocessing(text)
+    similarity = jaccard_similarity(title, text)
+    ranking = similarity_ranking(similarity)
+    summary = result(ranking, real_text, count)
+    return summary
