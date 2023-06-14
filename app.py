d49c7974e8faf8a0eae6f8951a5e6219b03f71a7
Krisna Dana <krisnadanaofficial@gmail.com>
2023-06-14 23:25:29 +0800

    add svd with dataset

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import jaccard
 import tf_idf
+import svd
 
 # flask --debug run
 app = Flask(__name__)
@@ -15,5 +16,6 @@ def submit():
     text = request.form['teks']
     count = request.form['jumlah']
     # result = jaccard.main(title, text, count)
-    result = tf_idf.main(title, text, count)
+    # result = tf_idf.main(title, text, count)
+    result = svd.main(title, text, count) # SVD (Singular Value Decomposition) 
     return render_template('index.html', judul=title, teks=text, jumlah=count, hasil=result)
